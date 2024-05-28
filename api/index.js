275cb1c5085935d9af333bb7999eff850334ca9c
ahmed <ahmedessam201790@gmail.com>
2024-05-28 18:21:42 +0300

    create db and server

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
+mongoose
+  .connect(process.env.DB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000!!!");
+  console.log(process.env.DB_URI);
 });
