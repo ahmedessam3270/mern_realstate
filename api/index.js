652220760f6adf26acfe676fe87e226ef5b67415
ahmed <ahmedessam201790@gmail.com>
2024-05-28 21:35:49 +0300

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+
 dotenv.config();
 mongoose
   .connect(process.env.DB_URI)
@@ -13,7 +15,13 @@ mongoose
   });
 const app = express();
 
+// by default express does not parse request bodies
+// we need to add a middleware to parse the request body
+// this middleware will parse the request body and add it to the request object
+app.use(express.json());
+
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(3000, () => {
   console.log(process.env.DB_URI);
