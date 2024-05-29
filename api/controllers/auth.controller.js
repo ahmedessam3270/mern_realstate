f87ea474bf0ff52504605da3ac3b9fb2c939f00c
ahmed <ahmedessam201790@gmail.com>
2024-05-29 08:05:40 +0300

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -10,5 +12,30 @@ export const signup = async (req, res, next) => {
     res.status(201).json("User created successfully");
   } catch (err) {
     next(err);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(401, "Wrong credentials"));
+    }
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Wrong credentials"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...restinfo } = validUser._doc;
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        expiresIn: process.env.JWT_EXPIRE,
+      })
+      .status(200)
+      .json(restinfo);
+  } catch (error) {
+    next(error);
   }
 };
