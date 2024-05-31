df622e27230fd3af62c59548f33606438c4887eb
ahmed <ahmedessam201790@gmail.com>
2024-05-31 06:16:26 +0300

    add create listing API route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
+
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -24,6 +26,7 @@ app.use(cookieParser());
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 // a middleware to handle possible errors
 app.use((err, req, res, next) => {
