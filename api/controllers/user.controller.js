4055877413ea217571f3098605e4f4881026f2ba
ahmed <ahmedessam201790@gmail.com>
2024-06-01 12:49:53 +0300

    complete show user listings API route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 import User from "../models/user.model.js";
+import Listing from "../models/listing.model.js";
 // User controller contains all the methods to handle user related operations.
 export const test = (req, res) => {
   res.json({ message: "User controller works" });
@@ -42,5 +43,18 @@ export const deleteUser = async (req, res, next) => {
     res.status(200).json("User has been successfully deleted!");
   } catch (error) {
     next(error);
+  }
+};
+
+export const getUserListings = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, "You can only view your own listings"));
   }
 };
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,6 +3,7 @@ import {
   test,
   updateUser,
   deleteUser,
+  getUserListings,
 } from "../controllers/user.controller.js";
 import { verifyUser } from "../utils/verifyUser.js";
 
@@ -13,5 +14,6 @@ const router = express.Router();
 router.get("/test", test);
 router.post("/update/:id", verifyUser, updateUser);
 router.delete("/delete/:id", verifyUser, deleteUser);
+router.get("/listings/:id", verifyUser, getUserListings);
 
 export default router;
